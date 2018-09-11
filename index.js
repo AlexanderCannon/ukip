361feaa2de9858354649f2805f0aeaf0b4d2a6c0
alexander-cannon <alexander.cannon@lloydsbanking.com>
2018-09-11 11:09:25 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const message = "don't be a racist!";
+console.log(message);
+
+//returns the message so you can add it to your own logs
+function ukip() {
+  return message;
+}
+
+module.exports = ukip;
